bf97bef3a0c027bce77695a473bb95674592d736
Radomir Mijovic <batomijovic4@gmail.com>
2022-01-23 15:01:39 +0100

    finished mobile nav

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -3,6 +3,7 @@ import {LinkH2, SidebarStyled} from "./SidebarStyled";
 import {AnimatePresence, motion} from "framer-motion";
 import {navbar_links} from "../Navbar/navbar_links";
 import {useStyleContext} from "../../context/style_context";
+import Image from "next/image";
 
 const Sidebar = () => {
     const {
@@ -24,18 +25,18 @@ const Sidebar = () => {
                 initial={{x: '-100%', opacity: 0}}
                 animate={{x: 0, opacity: 1}}
                 transition={{duration: .5}}
-                exit={{x: '-100%', opacity: 0}}
+                exit={{x: '100%', opacity: 0}}
                 as={motion.div}>
                 {navbar_links.map((item, index) => {
                     return (
-                        <div key={index} className='link'>
                             <LinkH2
+                                key={index}
                                 isActive={isActive}
                                 index={index}
                                 onClick={() => menuHandler(index)}>
-                                0{index} {item.title}
+                                {/*<Image src='/icons/gallery-icon2.gif' alt='gallery' width='50' height='50'/>*/}
+                                0{index} {item.title} <hr/>
                             </LinkH2>
-                        </div>
                     )
                 })}
             </SidebarStyled>
--- components/Sidebar/SidebarStyled.js
+++ components/Sidebar/SidebarStyled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const SidebarStyled = styled.div`
   width: 100vw;
   height: 100vh;
-  background: rgba(0, 0, 0, .7);
+  background: rgba(0, 0, 0, .8);
   //background: #E2001A;
   position: absolute;
   z-index: 7;
@@ -12,14 +12,17 @@ export const SidebarStyled = styled.div`
   justify-content: center;
   flex-direction: column;
   
-
   @media (min-width: 801px) {
     display: none;
   }
 `
 
 export const LinkH2 = styled.h2`
-  font-size: 3rem;
-  margin-bottom: 2.4rem;
+  font-size: 3.5rem;
+  margin-bottom: 3rem;
   color: ${props => props.isActive === props.index ? '#E2001A' : '#FFF'};
+  
+  > hr {
+    color: ${props => props.isActive === props.index ? '#E2001A' : '#FFF'};
+  }
 `
