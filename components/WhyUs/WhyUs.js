eecd4a885ee59b16460d1a99b156e084c89af9cf
Radomir Mijovic <batomijovic4@gmail.com>
2022-02-12 15:26:39 +0100

    why us folder prepared with files

---
+++ components/WhyUs/WhyUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {WhyUsStyled} from "./WhyUsStyled";
+
+const WhyUs = () => {
+    return (
+        <WhyUsStyled>
+
+        </WhyUsStyled>
+    );
+};
+
+export default WhyUs;
