632b50c4756875baabd2004ae9cced2c90ded77f
Radomir Mijovic <radomir.mijovic@productdna.com>
2022-06-03 09:46:20 +0200

    feat: added cloud storage config

---
+++ components/Gallery/MainGallery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {MainGalleryStyled} from "./MainGalleryStyled";
+
+const MainGallery = () => {
+    return (
+        <MainGalleryStyled>
+
+        </MainGalleryStyled>
+    );
+};
+
+export default MainGallery;
